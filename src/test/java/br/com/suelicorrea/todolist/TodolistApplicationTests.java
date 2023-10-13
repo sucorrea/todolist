f1a31e5b5533e7c47f9deba78b22af725688d1ec
sueli.oliveira <sueli.oliveira@newconsoftware.com.br>
2023-10-13 13:22:41 -0300

    inicio

---
+++ src/test/java/br/com/suelicorrea/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.suelicorrea.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
