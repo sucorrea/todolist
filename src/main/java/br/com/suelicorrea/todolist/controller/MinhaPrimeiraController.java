f9b274d14e17a8d267ca8c6cade0ee1af02fd9f4
sueli.oliveira <sueli.oliveira@newconsoftware.com.br>
2023-10-13 15:31:20 -0300

    primeira rota

---
+++ src/main/java/br/com/suelicorrea/todolist/controller/MinhaPrimeiraController.java
@@ -0,0 +1,15 @@
+package br.com.suelicorrea.todolist.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/primeiraRota")
+public class MinhaPrimeiraController {
+    @GetMapping("/")
+    public String primeiraMensagem() {
+        return "Funcionou";
+    }
+
+}
