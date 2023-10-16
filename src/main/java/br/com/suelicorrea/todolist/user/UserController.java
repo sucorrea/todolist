50d679c3e36a5c23a7045b700e117095ac329057
sueli.oliveira <sueli.oliveira@newconsoftware.com.br>
2023-10-16 19:16:10 -0300

    atualização

--- src/main/java/br/com/suelicorrea/todolist/user/UserController.java
+++ src/main/java/br/com/suelicorrea/todolist/user/UserController.java
@@ -1,16 +1,31 @@
 package br.com.suelicorrea.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.var;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
 
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var useCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(useCreated);
     }
 }
--- src/main/java/br/com/suelicorrea/todolist/user/UserModel.java
+++ src/main/java/br/com/suelicorrea/todolist/user/UserModel.java
@@ -1,10 +1,29 @@
 package br.com.suelicorrea.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    // @Column(name = "usuaro") caso queira mudar o nome no banco
+
+    @Column(unique = true)
     private String username;
+    
     private String name;
     private String password;
+    @CreationTimestamp
+    private LocalDateTime crearteAt;
 }
